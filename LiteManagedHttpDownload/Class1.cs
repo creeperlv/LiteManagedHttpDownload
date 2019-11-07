c20a0d71caa4688a1273de961cc4dd74c8bca818
Creeper Lv <1376021184@qq.com>
2019-11-07 10:19:54 +0800

    Basic Function

---
+++ LiteManagedHttpDownload/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace LiteManagedHttpDownload
+{
+    public class Downloader
+    {
+        public static async void DownloadToFileAsync(string Url,string path)
+        {
+            HttpClient httpClient = new HttpClient();
+            var s=await httpClient.GetByteArrayAsync(Url);
+            File.WriteAllBytes(path, s);
+        }
+    }
+}
