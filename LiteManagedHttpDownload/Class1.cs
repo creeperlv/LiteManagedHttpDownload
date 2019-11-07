2a50379cc623cd9ccf8b9b5438b9e99c6f2fd6a8
Creeper Lv <1376021184@qq.com>
2019-11-07 10:34:40 +0800

    Added more functions.

--- LiteManagedHttpDownload/Class1.cs
+++ LiteManagedHttpDownload/Class1.cs
@@ -12,5 +12,33 @@ namespace LiteManagedHttpDownload
             var s=await httpClient.GetByteArrayAsync(Url);
             File.WriteAllBytes(path, s);
         }
+        public static void DownloadToFileWithProgressAsync(string Url,string path,ref double Progress)
+        {
+            HttpClient httpClient = new HttpClient();
+            var s=(httpClient.GetStreamAsync(Url));
+            FileInfo fileInfo = new FileInfo(path);
+            var FW=fileInfo.OpenWrite();
+            s.Wait();
+            var st = s.Result;
+            byte[] b = new byte[64];
+            while (st.Position<st.Length)
+            {
+                st.Read(b, 0, b.Length);
+                FW.Write(b, 0, b.Length);
+                Progress = (double)st.Position / (double)st.Length;
+            }
+
+            //st.EndRead();
+            st.Close();
+            st.Dispose();
+            FW.Close();
+            FW.Dispose();
+        }
+        public static async void DownloadToFileFromBase64Async(string Url,string path)
+        {
+            HttpClient httpClient = new HttpClient();
+            var s=Convert.FromBase64String(await httpClient.GetStringAsync(Url));
+            File.WriteAllBytes(path, s);
+        }
     }
 }
