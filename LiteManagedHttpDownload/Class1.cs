d09d5819117a4b596dd26e2c99cac896069feb65
Creeper Lv <1376021184@qq.com>
2019-11-08 11:00:30 +0800

    Added exception check in primary function.

--- LiteManagedHttpDownload/Class1.cs
+++ LiteManagedHttpDownload/Class1.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace LiteManagedHttpDownload
 {
     public class Downloader
     {
 
-        public static async void DownloadToFileAsync(string Url,string path)
+        public static async Task<string> DownloadToFileAsync(string Url,string path)
         {
             HttpClient httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3952.0 Safari/537.36 Edg/80.0.320.3 LiteManagedHttpDownloader/1.0.0.0");
-            var s=await httpClient.GetByteArrayAsync(Url);
-            File.WriteAllBytes(path, s);
+            try
+            {
+
+                var s = await httpClient.GetByteArrayAsync(Url);
+                File.WriteAllBytes(path, s);
+                return "SUCCESS";
+            }
+            catch (Exception e)
+            {
+                return ""+e.Message;
+            }
         }
         public static void DownloadToFileWithProgressAsync(string Url,string path,ref double Progress)
         {
